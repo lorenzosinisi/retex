45b51e839156d924c3df8b6a458a395d8f456b02
Madpaia <mad.paia@gmail.com>
2019-11-10 15:27:26 +0100

    Adds timestamp to wme and specs

--- lib/wme.ex
+++ lib/wme.ex
@@ -1,6 +1,10 @@
 defmodule Retex.Wme do
-  @moduledoc "A working memory element, it represent the world in the form of identifier, attribute and values"
-  defstruct identifier: nil, attribute: nil, value: nil, id: nil
+  @moduledoc """
+    A working memory element, it represent the world in the form of identifier, attribute and values
+    timestamp is set at time of insertion into retex
+  """
+
+  defstruct identifier: nil, attribute: nil, value: nil, id: nil, timesptamp: nil
 
   def new(id, attr, val) do
     item = %__MODULE__{identifier: id, attribute: attr, value: val}
