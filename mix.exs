d896024f51d316cb0e64c6672ca73fa7b8d69283
lorenzosinisi <lasslo.net@gmail.com>
2020-01-20 20:39:13 +0100

    Prepare hex package meta

--- mix.exs
+++ mix.exs
@@ -1,20 +1,39 @@
 defmodule Retex.MixProject do
   use Mix.Project
-
+  @version "0.1.1"
   def project do
     [
       app: :retex,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
+      ddocs: [extras: ["README.md"], main: "readme", source_ref: "v#{@version}"],
+      source_url: "https://github.com/lorenzosinisi/retex",
+      description: description(),
+      package: package(),
       deps: deps()
     ]
+  end
+
+  defp description do
+    """
+    Rete algorithm in Elixir
+    """
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
+    ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Lorenzo Sinisi"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/lorenzosinisi/retex"}
     ]
   end
 
