2c2c612c721765b5e2a9f280ae93de5d7791ced0
lorenzosinisi <lasslo.net@gmail.com>
2020-01-30 13:31:27 +0100

    Fix tests

--- test/why_test.exs
+++ test/why_test.exs
@@ -2,7 +2,7 @@ defmodule Retex.WhyTest do
   use ExUnit.Case
   alias Retex.Facts
   import Facts
-  alias Retex.{Why, Node}
+  alias Retex.Why
 
   defp create_rule(lhs: given, rhs: action) do
     %{
@@ -17,13 +17,18 @@ defmodule Retex.WhyTest do
       has_attribute(:Family, :size, :==, "$a")
     ]
 
+    wme = Retex.Wme.new(:Account, :status, :ok)
+    wme_2 = Retex.Wme.new(:Family, :size, :ok)
+
     action = [
       {:Flight, :account_status, "$a"}
     ]
 
     rule = create_rule(lhs: given, rhs: action)
 
-    network = Retex.add_production(Retex.new(), rule)
+    network =
+      Retex.add_production(Retex.new(), rule) |> Retex.add_wme(wme) |> Retex.add_wme(wme_2)
+
     action = network.agenda |> List.first()
 
     assert "It exists an entity of type Account, with status == $a; " <>
