f5610d2207beb9a9db05a8a87df90914f24a40b1
Marco Milanesi <kpanic@gmail.com>
2023-07-04 16:18:41 +0200

    Correct file name

--- lib/agenda/strategy_behaviour.ex
+++ lib/agenda/strategy_behaviour.ex
@@ -1,6 +1,6 @@
 defmodule Retex.Agenda.Strategy do
   @moduledoc false
-  @type rules_executed :: List.t()
+  @type rules_executed :: list()
   @type network :: Retex.t()
 
   @callback consume_agenda(rules_executed, network) :: {:ok, {rules_executed, network}}
